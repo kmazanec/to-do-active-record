5c29e176c6b09f52ee7f641ec80572d305b2e143
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-13 16:23:47 -0500

    Added add and delete methods.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 
 class TasksController
   def self.add(new_task_description)
+    Task.create( description: new_task_description )
   end
 
   def self.delete(task_id_to_delete)
-
+    Task.delete(task_id_to_delete)
   end
 
   def self.complete!(task_id_to_complete)
