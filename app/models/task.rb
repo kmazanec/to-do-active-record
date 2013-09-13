fb68dbab8449335a248d3a865270136d6cc75b47
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-13 13:23:04 -0500

    added test and task object

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,13 @@
 class Task < ActiveRecord::Base
+  validates :description, presence: true
 
-  
+  def self.list
+    # select id, description from tasks
+    ary = []
+    self.all.each do |task|
+      ary << "#{task.id}). #{task.description}"
+    end
+  end
 end
 
 
