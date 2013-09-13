d564749f8c5ba911c0586c20ca8f87a8c930a126
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-13 12:32:36 -0500

    set up db migration

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
+
+  
 end
 
 
---
+++ db/migrate/20130913114100_create_list_table.rb
@@ -0,0 +1,9 @@
+class CreateListTable < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :description
+      t.integer :completed
+      t.timestamps
+    end
+  end
+end
