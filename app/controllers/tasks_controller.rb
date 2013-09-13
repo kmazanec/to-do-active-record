32b0adec56106e6d8db954fa446a740cee1f59e2
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-13 16:01:14 -0500

    added stuff to the task controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,17 @@
+
+class TasksController
+  def self.add(new_task_description)
+  end
+
+  def self.delete(task_id_to_delete)
+
+  end
+
+  def self.complete!(task_id_to_complete)
+
+  end
+
+  def self.list
+
+  end
+end
--- todo.rb
+++ todo.rb
@@ -1,3 +1,29 @@
 require_relative 'config/application'
 
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+# puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+#Check if user has passed arguments.
+  #IF not pass user list of commands.
+#Look at passed in command.
+  #IF "list" call list method.
+  #IF "add" add new record to database.
+  #IF "delete" delete appropriate record.
+  #IF "complete" mark task complete.
+
+first, second, third, rest = ARGV
+
+
+if ARGV.empty?
+  puts "Here are your options:"
+  puts "list"
+  puts "add <new task>"
+  puts "delete <task id>"
+  puts "complete <task id>"
+else
+  case ARGV.first
+  when "list" then puts TasksController.list
+  when "add" then TasksController.add(ARGV[1..-1].join(' '))
+  when "delete" then TasksController.delete(ARGV[1])
+  when "complete" then TasksController.complete(ARGV[1])
+  end
+end 
